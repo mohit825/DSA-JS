e27e892ce9aeab13c49aa187ce48c5149bf4d4df
mohit825 <mohit825@gmail.com>
2022-10-13 11:40:15 +0530

    Added level

--- Arrays/LeaderInArray.js
+++ Arrays/LeaderInArray.js
@@ -1,3 +1,4 @@
+// Level - Easy
 // Given an array A of positive integers. Your task is to find the leaders in the array. An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader.
 
 const leaderInArray = (arr, n) => {
--- Arrays/prefixSum/EquilibriumPoint.js
+++ Arrays/prefixSum/EquilibriumPoint.js
@@ -1,3 +1,5 @@
+// Level - Easy
+
 // Equilibrium Point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
 // Note: Retun the index of Equilibrium point. (1-based index)
 
